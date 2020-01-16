b6c56b1a6272f7f3a5efd917359967a84b8aa60d
Jordhan <jordhan.rdz@gmail.com>
2020-01-16 18:25:16 -0300

    Can select marker now

--- lib/widgets/location_input.dart
+++ lib/widgets/location_input.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:native_features/screens/map_screen.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 import '../helpers/location_helper.dart';
 
@@ -26,7 +27,7 @@ class _LocationInputState extends State<LocationInput> {
 
   Future<void> _selectOnMap() async {
     // when poped return a value
-    final selectedLocation = await Navigator.of(context).push(
+    final LatLng selectedLocation = await Navigator.of(context).push(
       MaterialPageRoute(
         fullscreenDialog: true,
         builder: (ctx) => MapScreen(
@@ -38,6 +39,7 @@ class _LocationInputState extends State<LocationInput> {
     if (selectedLocation == null) {
       return;
     }
+    print(selectedLocation.latitude);
   }
 
   @override
